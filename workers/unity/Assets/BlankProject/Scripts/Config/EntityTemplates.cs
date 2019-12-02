dc61068a93d24f95c88c340dc99f7081c32dbcca
Judah Perez <judah@cookiedragon.games>
2019-12-01 17:04:28 -0800

    Schema import

--- workers/unity/Assets/BlankProject/Scripts/Config/EntityTemplates.cs
+++ workers/unity/Assets/BlankProject/Scripts/Config/EntityTemplates.cs
@@ -1,6 +1,8 @@
+using System.Collections.Generic;
 using Improbable;
 using Improbable.Gdk.Core;
 using Improbable.Gdk.PlayerLifecycle;
+using Improbable.Item;
 
 namespace BlankProject.Scripts.Config
 {
@@ -11,8 +13,12 @@ namespace BlankProject.Scripts.Config
             var clientAttribute = EntityTemplate.GetWorkerAccessAttribute(workerId);
             var serverAttribute = UnityGameLogicConnector.WorkerType;
 
+            var inv = new Dictionary<int, ItemData>();
+
+
             var template = new EntityTemplate();
             template.AddComponent(new Position.Snapshot(), clientAttribute);
+            template.AddComponent(new Inventory.Snapshot(inv, 24, 1), clientAttribute);
             template.AddComponent(new Metadata.Snapshot("Player"), serverAttribute);
 
             PlayerLifecycleHelper.AddPlayerLifecycleComponents(template, workerId, serverAttribute);
